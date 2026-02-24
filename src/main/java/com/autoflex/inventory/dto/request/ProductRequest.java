e6fbab04b9b953ee49113793e1bf7772b26ac0fb
John Mota <johnmarcos.mota@gmail.com>
2026-02-24 19:00:11 -0300

    fix: ajuste para adicionar ProductRawMateria no request

--- src/main/java/com/autoflex/inventory/dto/request/ProductRequest.java
+++ src/main/java/com/autoflex/inventory/dto/request/ProductRequest.java
@@ -1,10 +1,12 @@
 package com.autoflex.inventory.dto.request;
 
+import com.autoflex.inventory.dto.response.ProductRawMaterialResponse;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public record ProductRequest(
     @NotBlank(message = "O nome do produto é obrigatório")
@@ -12,6 +14,8 @@ public record ProductRequest(
 
     @NotNull(message = "O valor do produto é obrigatório")
     @Positive(message = "O valor do produto deve ser positivo")
-    BigDecimal value
+    BigDecimal value,
+
+    List<ProductRawMaterialResponse>rawMaterials
 ) {
 }
