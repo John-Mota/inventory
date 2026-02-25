6fe3a19e962561552bfb9a36e50d594b434d6d38
John Mota <johnmarcos.mota@gmail.com>
2026-02-25 12:21:41 -0300

    fix: Atualiza dependência do Lombok

--- src/main/java/com/autoflex/inventory/config/OpenApiConfig.java
+++ src/main/java/com/autoflex/inventory/config/OpenApiConfig.java
@@ -1,6 +1,6 @@
 package com.autoflex.inventory.config;
 
-import io.r.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
